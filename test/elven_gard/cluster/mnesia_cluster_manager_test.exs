131c5d6f034e1b73d0b0040e0f4a1c06e55a883e
ImNotAVirus <17680522+ImNotAVirus@users.noreply.github.com>
2023-09-17 11:35:11 +0200

    :sparkles: Add timeout for spawning processes

--- test/elven_gard/cluster/mnesia_cluster_manager_test.exs
+++ test/elven_gard/cluster/mnesia_cluster_manager_test.exs
@@ -3,7 +3,8 @@ defmodule ElvenGard.Cluster.MnesiaClusterManagerTest do
   use ElvenGard.Cluster.Testing.ExUnitCluster
 
   alias ElvenGard.Cluster.MnesiaClusterManager
-  alias ElvenGard.Cluster.Testing.LocalCluster
+
+  @timeout 5_000
 
   ## Tests
 
@@ -11,28 +12,34 @@ defmodule ElvenGard.Cluster.MnesiaClusterManagerTest do
   cluster "toto", %{nodes: nodes} do
     # Start MnesiaClusterManager
     opts = [auto_connect: false, retry_interval: 1]
-    :ok = multi_spawn_request(nodes, MnesiaClusterManager, opts)
+    :ok = multi_spawn_request!(nodes, MnesiaClusterManager, opts)
 
     :rpc.multicall(nodes, MnesiaClusterManager, :connected?, []) |> IO.inspect()
   end
 
   ## Helpers
 
-  defp multi_spawn_request(nodes, module, opts) do
+  defp now(), do: System.monotonic_time(:millisecond)
+
+  defp multi_spawn_request!(nodes, module, opts) do
     Enum.map(nodes, &Node.spawn_link(&1, module, :start_link, [opts]))
 
     # Here this case is pretty weird:
     # start_link is supposed to be synchronous but the function return before
     # the process being spawned. So I need to wait for local name registration
-    wait_for_spawn(nodes, module)
+    wait_for_spawn!(nodes, module, @timeout)
   end
 
-  defp wait_for_spawn(nodes, module) do
+  defp wait_for_spawn!(nodes, module, timeout, start \\ now()) do
+    if now() >= start + timeout do
+      raise "timeout: #{inspect(module)} not found on all nodes #{inspect(nodes)}"
+    end
+
     {result, []} = :rpc.multicall(nodes, Process, :whereis, [module])
 
     case Enum.all?(result) do
       true -> :ok
-      false -> wait_for_spawn(nodes, module)
+      false -> wait_for_spawn!(nodes, module, timeout, start)
     end
   end
 end
